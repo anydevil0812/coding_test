b623ca8d33fc1209c597201176f1c0fe848ccea1
anydevil0812 <109947297+anydevil0812@users.noreply.github.com>
2022-08-01 22:07:39 +0900

    Delete 12918.py

--- 12918.py
+++
@@ -1,7 +0,0 @@
-# https://programmers.co.kr/learn/courses/30/lessons/12918","문자열 다루기 기본"
-def solution(s):
-    if (len(s) == 4 or len(s) == 6) and s.isdigit() == True:
-        answer = True
-    else:
-        answer = False
-    return answer
