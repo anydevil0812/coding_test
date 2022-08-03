62c7c5efbdf23e2e448d9998e27b93b4606e3ad9
anydevil0812 <anydevil0812@naver.com>
2022-08-03 20:18:40 +0900

    프로그래머스 문제

---
+++ 프로그래머스/하샤드 수.py
@@ -0,0 +1,9 @@
+def solution(x):
+    a = 0
+    for i in range(len(str(x))):
+        a += int(str(x)[i])
+        if x % a == 0:
+            answer = True
+        else:
+            answer = False
+    return answer
