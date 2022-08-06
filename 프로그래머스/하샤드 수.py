5511d979420edfb2da0ff4fb6e57c5a5781b024d
anydevil0812 <109947297+anydevil0812@users.noreply.github.com>
2022-08-06 21:04:30 +0900

    Delete 하샤드 수.py

--- 프로그래머스/하샤드 수.py
+++
@@ -1,9 +0,0 @@
-def solution(x):
-    a = 0
-    for i in range(len(str(x))):
-        a += int(str(x)[i])
-        if x % a == 0:
-            answer = True
-        else:
-            answer = False
-    return answer
