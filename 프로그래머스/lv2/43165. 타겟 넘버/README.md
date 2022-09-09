3ebf899a5d7536106d15a1429d568af51081b2e3
anydevil0812 <109947297+anydevil0812@users.noreply.github.com>
2022-09-09 21:35:26 +0900

    [level 2] Title: 타겟 넘버, Time: 2.50 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/README.md
+++ 프로그래머스/lv2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 2.46 ms
+메모리: 10.2 MB, 시간: 2.50 ms
 
 ### 구분
 
--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -5,8 +5,8 @@ def solution(numbers, target):
     return count
 
 def dfs(numbers, target, index, sum):
-    global count
-    if index == len(numbers):
+    global count # global을 하지 않고 count를 함수의 원소 중 하나로 적용하면 재귀함수를 실행할때마다 count가 초기값인 0으로만 적용됨
+    if index == len(numbers): # sum에 numbers의 원소의 개수만큼 연산을 진행하겠다는 의미 
         if sum == target:
             count += 1
         return
