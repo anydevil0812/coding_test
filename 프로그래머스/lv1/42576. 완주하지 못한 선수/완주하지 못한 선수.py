e482b7a49e0acd5ae5a7214dcb24ba3986d031d3
anydevil0812 <anydevil0812@naver.com>
2022-09-10 01:56:50 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 42.95 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -2,8 +2,8 @@ def solution(participant, completion):
     a = {}
     hashsum = 0
     for i in participant:
-        a[hash(i)] = i
-        hashsum += hash(i)
-    for j in completion:
+        a[hash(i)] = i # 선수 이름을 value로 가지는 hash값을 key로 지정
+        hashsum += hash(i) # 모든 해쉬값 합치기
+    for j in completion: # 합쳐진 해쉬값에서 완주에 성공한 선수들의 해쉬값 제거
         hashsum -= hash(j)
-    return a[hashsum]   
+    return a[hashsum]  # 결국 남은 해쉬값은 완주하지 못한 선수에 대한 해쉬값이므로 이를 이용하여 선수 이름 반환
