696e34e0d3caec0de4034eaf9d8f9555eea9ffdd
anydevil0812 <anydevil0812@naver.com>
2022-09-09 22:25:33 +0900

    [level 2] Title: 타겟 넘버, Time: 2.47 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -4,7 +4,7 @@ def solution(numbers, target):
     return count 
 
 def dfs(numbers, target, index, sum): 
-    global count # global로 지정하지 않으면 재귀함수 실행할때마다 count가 계속 초기값인 0으로 적용됨
+    global count # global로 지정하지 않으면 재귀함수 실행할 때마다 count가 계속 초기값인 0으로 적용됨
     if index == len(numbers): # numbers의 길이만큼 sum에 연산을 해줌
         if sum == target:
             count += 1
