54b4fd18dd4e52b493bc5091b9e233ab5d4e5d44
anydevil0812 <anydevil0812@naver.com>
2022-09-12 22:37:13 +0900

    [Silver I] Title: 단지번호붙이기, Time: 68 ms, Memory: 31084 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -18,22 +18,22 @@ def dfs(x, y):
         dfs(x-1, y)
         dfs(x, y+1)
         dfs(x, y-1)
-        return count
+        return count # count = 한 단지에서의 집의 수
 
     else:
         return False
 
-block = []
+block = [] # 단지의 개수 파악을 위해 빈 리스트 생성
 
 for j in range(size):
     for k in range(size):
         if dfs(j, k) > 0:
             block.append(count)
-            count = 0
+            count = 0 # 한 단지에서의 집의 수 파악 후 다음 단지에서의 집의 수 파악을 위해 count 0으로 초기화
 
-print(len(block))
+print(len(block)) # 단지 수 출력
 
-block.sort()
+block.sort() # 오름차순 출력을 위해 sort
 
 for n in block:
-    print(n)
+    print(n) # 각 단지에서의 집의 수 출력
