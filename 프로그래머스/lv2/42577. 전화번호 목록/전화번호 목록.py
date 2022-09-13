db296dde87712a2e5575648baead7a4b5f5b27a0
anydevil0812 <109947297+anydevil0812@users.noreply.github.com>
2022-09-13 19:30:52 +0900

    [level 2] Title: 전화번호 목록, Time: 97.38 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,10 +1,7 @@
 a = []
 def solution(phone_book):
-    phone_book.sort()
+    phone_book.sort() # phone_book의 원소들은 int가 아니라 str이기 때문에 sort()를 적용하면 비슷한 번호의 원소들 앞 순서에 정렬됨  
     for i, j in zip(phone_book, phone_book[1:]):
             if i == j[0:len(i)]:
                 return False          
-            else:
-                a.append(True)
-    if True in a:
-        return True
+    return True
