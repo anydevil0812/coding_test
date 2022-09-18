05123dc8121c3443ca0ad3fce757c87427f724bc
anydevil0812 <109947297+anydevil0812@users.noreply.github.com>
2022-09-18 20:17:12 +0900

    [level 1] Title: 폰켓몬, Time: 0.58 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,7 +1,7 @@
 def solution(nums):
-    a = len(nums)/2
-    li = list(dict.fromkeys(nums))
-    b = len(li)
+    a = len(nums)/2 # 가져갈 수 있는 폰켓몬 수
+    li = list(dict.fromkeys(nums)) # 폰켓몬 종류 중복 제거
+    b = len(li) # 가져갈 수 있는 폰켓몬의 종류수 
     if a <= b:
         return a
     else:
