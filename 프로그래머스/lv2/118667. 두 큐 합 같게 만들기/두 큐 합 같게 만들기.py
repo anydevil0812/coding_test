9e3742a1097880fcb9a8c929999f09a32c53fc15
anydevil0812 <109947297+anydevil0812@users.noreply.github.com>
2022-10-23 23:06:39 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 46.62 ms, Memory: 19.3 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -4,19 +4,20 @@ def solution(queue1, queue2):
     queue1, queue2 = deque(queue1), deque(queue2)
     sum1, sum2 = sum(queue1), sum(queue2)
     
-    for i in range(len(queue1) *3):
+    for i in range(len(queue1) * 3):
         if sum1 > sum2:
             sum1 -= queue1[0]
             sum2 += queue1[0]
             queue2.append(queue1.popleft())
-            
+            answer += 1
             
         elif sum1 < sum2:
             sum1 += queue2[0]
             sum2 -= queue2[0]
             queue1.append(queue2.popleft())
+            answer += 1
+            
         else:
             return answer
-        answer += 1
         
     return -1
