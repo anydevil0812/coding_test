89b1a3c2a6d817b0849b3ffd08350e4520f80d9e
anydevil0812 <109947297+anydevil0812@users.noreply.github.com>
2022-10-30 23:38:19 +0900

    [level 2] Title: 가장 큰 수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,13 +1,15 @@
 def solution(numbers):
-    if max(numbers) == 0:
+    if sum(numbers) == 0:
         return "0"
+    
     li = []
     for i in range(len(numbers)):
-        b = (str(numbers[i]) * 4)[:4]
-        c = len(str(numbers[i]))
-        li.append((b,c))
+        a = (str(numbers[i]) * 4)[:4]
+        b = len(str(numbers[i]))
+        li.append((a,b))
+        
     li.sort(reverse=True) 
     answer = ''
-    for j, k in li:
-        answer += j[:k]
+    for a, b in li:
+        answer += a[:b]
     return answer
