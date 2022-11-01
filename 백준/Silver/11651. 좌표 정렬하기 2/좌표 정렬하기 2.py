f0f37dd0466d1fb21936e285ec3c7fc6c6874ce2
anydevil0812 <109947297+anydevil0812@users.noreply.github.com>
2022-11-01 21:49:40 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 4460 ms, Memory: 44932 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -1,8 +1,9 @@
 a = int(input())
 li =[]
 for i in range(a):
-    li.append(list(map(int, input().split())))
+    b, c = list(map(int, input().split()))
+    li.append([c, b])
 
-li.sort(key=lambda x:(x[1],x[0]))
-for j in range(len(li)):
-    print(li[j][0], li[j][1])
+li.sort()
+for c, b in li:
+    print(b, c)
