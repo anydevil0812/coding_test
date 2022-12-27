5b64ab5a2328e70c0e1c73133273083e742e39df
anydevil0812 <109947297+anydevil0812@users.noreply.github.com>
2022-12-27 23:59:14 +0900

    [Silver II] Title: 좌표 압축, Time: 1864 ms, Memory: 150180 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -0,0 +1,7 @@
+import sys
+n = int(sys.stdin.readline())
+li = list(map(int, sys.stdin.readline().split()))
+li2 = sorted(list(set(li)))
+dic = {li2[i] : i for i in range(len(li2))}
+for i in li:
+    print(dic[i], end = ' ')
