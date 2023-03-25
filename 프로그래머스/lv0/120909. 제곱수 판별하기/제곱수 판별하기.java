458fc6f8aed66daaf9f1b6b4c713c56b13eae4a6
anydevil0812 <109947297+anydevil0812@users.noreply.github.com>
2023-03-25 13:49:13 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,10 +1,6 @@
 class Solution {
     public int solution(int n) {
         double a = Math.sqrt(n);
-        if(a == (int)a){
-            return 1;
-        }else{
-            return 2;
-        }
+        return (a == (int)a) ? 1 : 2;
     }
 }
