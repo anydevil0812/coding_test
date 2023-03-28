16860af35f16a6d9631c51acfe8846b9a5904828
anydevil0812 <109947297+anydevil0812@users.noreply.github.com>
2023-03-28 17:13:38 +0900

    [level 0] Title: 배열 자르기, Time: 0.01 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
@@ -2,11 +2,6 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
-        ArrayList<Integer> li = new ArrayList<>();
-        for(int i = num1; i <= num2; i++){
-            li.add(numbers[i]);
-        }
-        int[] answer = li.stream().mapToInt(i -> i).toArray();
-        return answer;
+        return Arrays.copyOfRange(numbers, num1, num2+1);
     }
 }
