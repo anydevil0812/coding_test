ea1b1b051f2fbc0b2ff862fa58b824e085b5de02
anydevil0812 <anydevil0812@naver.com>
2023-11-14 17:16:35 +0900

    [level 3] Title: N으로 표현, Time: 25.22 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int N, int number) {
+        ArrayList<Set<Integer>> li = new ArrayList<>();
+        for(int i = 0; i <= 8; i++){
+            li.add(new HashSet<>());
+        }
+	    li.get(1).add(N);
+        
+        for(int j = 2; j <= 8; j++){
+            Set<Integer> countSet = li.get(j);
+            
+            for(int k = 1; k <= j; k++){
+                Set<Integer> set1 = li.get(k);
+                Set<Integer> set2 = li.get(j-k);
+                
+                for(int num1 : set1){
+                    for(int num2 : set2){
+                        countSet.add(num1 + num2);
+                        countSet.add(num1 - num2);
+                        countSet.add(num1 * num2);
+                        if(num1 != 0 && num2 != 0){
+                            countSet.add(num1 / num2);
+                        }
+                    }
+                }   
+            }
+            countSet.add(Integer.parseInt(String.valueOf(N).repeat(j)));
+        }
+        
+        for(Set<Integer> set : li){
+            if(set.contains(number)){
+                return li.indexOf(set);
+            }
+        }   
+        return -1;
+    }
+}
